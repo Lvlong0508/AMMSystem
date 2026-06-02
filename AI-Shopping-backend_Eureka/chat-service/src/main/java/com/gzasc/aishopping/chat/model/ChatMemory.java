c28a00d5a0b949a0b03b3f892a736792bd7c9220
lv1ong <3393469550@qq.com>
2026-06-02 21:00:14 +0800

    删除了一些无用文件,将maxMessages改成配置获取

--- AI-Shopping-backend_Eureka/chat-service/src/main/java/com/gzasc/aishopping/chat/model/ChatMemory.java
+++ AI-Shopping-backend_Eureka/chat-service/src/main/java/com/gzasc/aishopping/chat/model/ChatMemory.java
@@ -2,16 +2,20 @@ package com.gzasc.aishopping.chat.model;
 
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChatMemory {
 
+    @Value("${chat.memory.maxMessages}")
+    private int maxMessages;
+
     @Bean
     public ChatMemoryProvider chatMemoryProvider() {
         return memoryId -> MessageWindowChatMemory.builder()
-                .maxMessages(10)
+                .maxMessages(maxMessages)
                 .id(memoryId)
                 .build();
     }
